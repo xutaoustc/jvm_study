edfff3d91c469bfb81cd35d4e3ea64c49a912838
xutao <xutao_ustc@163.com>
2020-05-08 13:55:28 +0800

    增加注解的注释

--- src/main/java/annotation/MyAnnotationTest.java
+++ src/main/java/annotation/MyAnnotationTest.java
@@ -5,9 +5,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+
+// @Target, @Retention, @Document, @Inherited 4种元注解
 @Target(value={ElementType.METHOD,ElementType.TYPE})
 @Retention(value=RetentionPolicy.RUNTIME)
 @interface MyAnnotation {
+    // 可以使用default给属性设置默认的初始值
+    // 如果只有一个属性，而且属性的名字为value，则在使用时可以不使用属性名称
     String value() default "";
 }
 
